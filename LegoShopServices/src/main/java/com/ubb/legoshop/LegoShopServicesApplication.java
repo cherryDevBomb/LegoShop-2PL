af7a5d03c0d8fe1ca40973ee7b109d64a860265f
cherryDevBomb <turcan.olguta@gmail.com>
2021-11-27 18:14:04 +0200

    first commit

---
+++ LegoShopServices/src/main/java/com/ubb/legoshop/LegoShopServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ubb.legoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoShopServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoShopServicesApplication.class, args);
+    }
+
+}
